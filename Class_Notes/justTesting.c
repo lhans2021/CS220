ffbc5241cb928a60cb319896adce6a0f7de4c030
Hans Li <hli221@binghamton.edu>
2023-02-26 01:52:53 -0500

    notes updated again

---
+++ Class_Notes/justTesting.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (int argc, char **argv) {
+    // int i;
+    // for (int i = 1; i < argc; i++) {
+    //     printf("argv[%d] = %s ", i, argv[i]);
+        
+    // }
+    // printf("\n");
+    // return 0;
+
+    printf("argv[0] = %s", argv[0]);
+}
